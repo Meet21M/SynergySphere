25329f2296117a7eb1814d480711b09c4d7603c9
Soul-Will <ttry7407@gmail.com>
2025-09-06 15:40:54 +0530

    User Registration API Added

--- backend/config.py
+++ backend/config.py
@@ -2,3 +2,7 @@ from dotenv import load_dotenv
 import os
 load_dotenv()
 DB_URL = os.environ.get("DB_URL")
+SENDER_MAIL = os.environ.get("SENDER_MAIL")
+PASSWORD = os.environ.get("PASSWORD")
+ALGORITHM = os.environ.get("ALGORITHM")
+SECRET_KEY = os.environ.get("SECRET_KEY")
